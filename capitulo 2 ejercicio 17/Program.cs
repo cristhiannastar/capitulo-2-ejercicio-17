21b210d86cd89fc38b3d89eac91016080d376f88
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-16 22:08:25 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 17/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ejerc217
+{
+
+    public class program
+    {
+
+        public static void Main()
+        {
+            int i;
+            for (i = 0; i <= 10; i++)
+            {
+                double resultado = Math.Pow(2, 1);
+                Console.WriteLine(resultado);
+            }
+
+        }
+    }
+}
